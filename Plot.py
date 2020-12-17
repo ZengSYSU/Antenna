e1a036db7863d937be12af5a75836078fb4dd47f
ZengSYSU <zengjh26@mail2.sysu.edu.cn>
2020-12-17 23:15:11 +0800

    2D Plot

--- Plot.py
+++ Plot.py
@@ -10,24 +10,27 @@ os.chdir(_path)
 sp = pd.read_csv('S Parameter Plot 1.csv')
 gain = pd.read_csv('Gain Plot 2.csv')
 freq, dB = [], []
-freq.append(sp.iloc[0:, 0])
-dB.append(sp.iloc[0:, 1])
+freq.append(sp.iloc[:, 0])
+dB.append(sp.iloc[:, 1])
 
 plt.subplot(211)
 plt.plot(freq, dB, color='red', marker='.', lw=5)
 plt.xlabel('Freq[GHz]')
 plt.ylabel('dB(S(1,1))')
 
-theta, r, n = [], [], 180
+theta, E = [], []
 theta.append((gain.iloc[0:, 0]))
-r.append(gain.iloc[0:, 1])
-# theta = np.arange(0, 2*np.pi, 2*np.pi / n)
-print(r, theta)
+for index in range(len(theta)):
+    theta[index] = theta[index]*np.pi/180
+E.append(gain.iloc[0:, 1])
+print(theta, E)
+
 ax = plt.subplot(212, projection='polar')
 ax.set_theta_direction(-1)
 ax.set_theta_zero_location('N')
 ax.set_thetagrids(np.arange(0, 360, 30))
-plt.plot(theta, r,  marker='.', color='black', lw=2)
+plt.polar(theta, E,  marker='.', color='black', lw=2)
+
 plt.title('Pattern', fontsize=10)
 plt.tight_layout()
 plt.show()
--- test.py
+++
@@ -1,9 +0,0 @@
-# ----------------------------------------------
-# Script Recorded by ANSYS Electronics Desktop Version 2018.0.0
-# 20:30:06  11月 19, 2020
-# ----------------------------------------------
-import ScriptEnv
-ScriptEnv.Initialize("Ansoft.ElectronicsDesktop")
-oDesktop.RestoreWindow()
-oProject = oDesktop.NewProject()
-oProject.InsertDesign("HFSS", "HFSSDesign1", "DrivenModal", "")
