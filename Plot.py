bfefbf61a810a328f2d523d51d665d4a84eead7e
ZengSYSU <zengjh26@mail2.sysu.edu.cn>
2020-12-24 17:08:03 +0800

    2D Plot

--- Plot.py
+++ Plot.py
@@ -2,8 +2,8 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
-import os
 import math
+import os
 
 _path = os.getcwd()
 os.chdir(_path)
@@ -20,9 +20,9 @@ plt.xlabel('Freq[GHz]')
 plt.ylabel('dB(S(1,1))')
 
 theta = gain.iloc[:, 0]
-for index in range(len(theta)):
-    theta[index] = round(theta[index]*math.pi/180)
-print(theta)
+theta_f = theta.astype(np.float)
+for index in range(len(theta_f)):
+    theta_f[index] = math.radians(theta_f[index])
 E = gain.iloc[:, 1]
 r = _gain.iloc[:, 1]
 
@@ -30,11 +30,11 @@ ax = plt.subplot(223, projection='polar')
 ax.set_theta_direction(-1)
 ax.set_theta_zero_location('N')
 ax.set_thetagrids(np.arange(0, 360, 30))
-plt.polar(theta, E, color='black', lw=2)
+plt.polar(theta_f, E, color='black', lw=2)
 plt.title('Pattern', fontsize=10)
 
 plt.subplot(224, projection='polar')
-plt.polar(theta, r, color='green', lw=2)
+plt.polar(theta_f, r, color='green', lw=2)
 plt.title('Pattern', fontsize=10)
 # plt.tight_layout()
 plt.show()
