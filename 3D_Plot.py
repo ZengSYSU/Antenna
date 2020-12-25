26becd2100b08e3823d7d3ddf0df5325d1973ece
ZengSYSU <zengjh26@mail2.sysu.edu.cn>
2020-12-25 14:24:38 +0800

    3D Plot

--- 3D_Plot.py
+++ 3D_Plot.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
+import math
 from mayavi import mlab
 import os
 
@@ -10,18 +11,22 @@ gain = pd.read_csv('Gain Plot 1.csv')
 
 Phi = gain.iloc[:, 0]
 phi = Phi.astype(np.float)
+for index in range(len(phi)):
+    phi[index] = math.radians(phi[index])
 Theta = gain.iloc[:, 1]
 theta = Theta.astype(np.float)
+for index in range(len(theta)):
+    theta[index] = math.radians(theta[index])
 
 dB_min = np.min(gain.iloc[:, 2])
 scale = abs(dB_min) + 1
 dB = gain.iloc[:, 2] + scale
 
 phi, theta = np.meshgrid(phi, theta)
-r = dB * np.sin(theta)
+r = 1 * np.sin(theta)
 x = r * np.cos(phi)
 y = r * np.sin(phi)
-z = dB * np.cos(theta)
+z = 1 * np.cos(theta)
 
 mlab.mesh(x, y, z)
 mlab.show()
